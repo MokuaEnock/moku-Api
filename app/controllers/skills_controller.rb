4d7b5c9d74127e48e433df1c24a79b7dd254089e
Enock <enock.mokua@student.moringaschool.com>
2023-01-13 08:06:03 +0300

    create actions for skills

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,2 +1,16 @@
 class SkillsController < ApplicationController
+  def index
+    render json: Skill.all
+  end
+
+  def create
+    skill = Skill.create(skill_params)
+    render json: skill
+  end
+
+  private
+
+  def skil_params
+    params.permit(:name, :description)
+  end
 end
