4c9c2d10af717d78d4e6a0eee6782529a9112a2b
Enock <enock.mokua@student.moringaschool.com>
2023-01-13 07:06:42 +0300

    add attributes to messages

--- db/migrate/20230113033135_create_messages.rb
+++ db/migrate/20230113033135_create_messages.rb
@@ -2,6 +2,9 @@ class CreateMessages < ActiveRecord::Migration[7.0]
   def change
     create_table :messages do |t|
       t.belongs_to :project, foreign_key: true
+      t.string :name
+      t.string :email 
+      t.string :description
 
       t.timestamps
     end
