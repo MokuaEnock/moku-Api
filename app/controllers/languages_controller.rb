ae4f74afc6971192ed322c349ecea09f4463ad28
Enock <enock.mokua@student.moringaschool.com>
2023-01-13 07:40:11 +0300

    create language actions

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -1,2 +1,16 @@
 class LanguagesController < ApplicationController
+  def index
+    render json: Language.all
+  end
+
+  def create
+    lan = User.create(lan_params)
+    render json: lan
+  end
+
+  private
+
+  def lan_params
+    params.permit(:name, :decription)
+  end
 end
