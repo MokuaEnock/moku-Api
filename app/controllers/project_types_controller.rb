dfe7a11fdc8763d2ea04c89656874f09a996ab92
Enock <enock.mokua@student.moringaschool.com>
2023-01-15 08:31:40 +0300

    render and create project type

--- app/controllers/project_types_controller.rb
+++ app/controllers/project_types_controller.rb
@@ -1,2 +1,16 @@
 class ProjectTypesController < ApplicationController
+  def index
+    render json: ProjectType.all
+  end
+
+  def create
+    pro = ProjectType.create(type_s)
+    render json: pro
+  end
+
+  private
+
+  def type_s
+    params.permit(:name, :description)
+  end
 end
