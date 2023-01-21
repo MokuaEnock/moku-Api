f2a1e07384aea3839fa4b9f07c2bcc861759c7ef
Enock <enock.mokua@student.moringaschool.com>
2023-01-21 10:18:04 +0300

    add attributes to roles

--- db/migrate/20230121071525_create_roles.rb
+++ db/migrate/20230121071525_create_roles.rb
@@ -1,7 +1,8 @@
 class CreateRoles < ActiveRecord::Migration[7.0]
   def change
     create_table :roles do |t|
-
+      belongs_to :experience, foreign_key: true
+      t.string :description
       t.timestamps
     end
   end
