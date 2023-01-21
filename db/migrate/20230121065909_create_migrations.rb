4ee206bece7b282a40bf780d928dc4a40beda725
Enock <enock.mokua@student.moringaschool.com>
2023-01-21 10:00:26 +0300

    add users attributes

--- db/migrate/20230121065909_create_migrations.rb
+++ db/migrate/20230121065909_create_migrations.rb
@@ -2,7 +2,9 @@ class CreateMigrations < ActiveRecord::Migration[7.0]
   def change
     create_table :migrations do |t|
       t.string :Role
-
+      t.string :name
+      t.string :description
+      
       t.timestamps
     end
   end
