1b9587cc13f633340f1dffdbcfcf04747749c93c
Enock <enock.mokua@student.moringaschool.com>
2023-01-21 12:02:31 +0300

    permit parasms for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,26 @@
 class ProjectsController < ApplicationController
+  def index
+    render json: Project.all
+  end
+
+  def show
+    project = Project.find(params[:id])
+    render json: project
+  end
+
+  private
+
+  def project_params
+    params.permit(
+      :name,
+      :description,
+      :short_description,
+      :project_start,
+      :project_end,
+      :github_link,
+      :live_link,
+      :status,
+      :field
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
   resources :skills
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  namespace :api do
+    namespace :v1 do
+    end
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
