c787607b24446b5688a386852978407e27740c21
Enock <enock.mokua@student.moringaschool.com>
2023-01-21 10:15:37 +0300

    create role resource

--- db/migrate/20230121070233_create_experiences.rb
+++ db/migrate/20230121070233_create_experiences.rb
@@ -2,12 +2,13 @@ class CreateExperiences < ActiveRecord::Migration[7.0]
   def change
     create_table :experiences do |t|
       t.belongs_to :role, foreign_key: true
+      t.string :title
       t.string :location
       t.string :company_name
       t.string :company_link
       t.date :start_date
       t.date :end_date
-      
+
 
       t.timestamps
     end
