779e9cd159ad6d1e06d0dec94b0f35ab84a386bb
Enock <enock.mokua@student.moringaschool.com>
2023-01-22 13:37:28 +0300

    create actions for tags controller

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,2 +1,16 @@
 class TagsController < ApplicationController
+  def index
+    render json: Tag.all
+  end
+
+  def create
+    tag = Tag.create(tag_params)
+    render json: tag
+  end
+
+  private
+
+  def tag_params
+    params.permit(:name, :description)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -90,6 +90,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_22_102206) do
   end
 
   create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
