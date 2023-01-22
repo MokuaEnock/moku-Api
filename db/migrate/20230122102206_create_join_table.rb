f1224e4072fce6081b3bc3f7c1822f9128358ceb
Enock <enock.mokua@student.moringaschool.com>
2023-01-22 13:22:40 +0300

    create join table for tags and projects

---
+++ db/migrate/20230122102206_create_join_table.rb
@@ -0,0 +1,8 @@
+class CreateJoinTable < ActiveRecord::Migration[7.0]
+  def change
+    create_join_table :Projects, :Tags do |t|
+      # t.index [:project_id, :tag_id]
+      # t.index [:tag_id, :project_id]
+    end
+  end
+end
