d98d06494511fe860f6d185b2b8f4d84bb1686cd
Enock <enock.mokua@student.moringaschool.com>
2023-01-22 13:23:53 +0300

    add attribute to Tag and Projct join table

--- db/migrate/20230122102206_create_join_table.rb
+++ db/migrate/20230122102206_create_join_table.rb
@@ -3,6 +3,8 @@ class CreateJoinTable < ActiveRecord::Migration[7.0]
     create_join_table :Projects, :Tags do |t|
       # t.index [:project_id, :tag_id]
       # t.index [:tag_id, :project_id]
+      t.index :project_id
+      t.index :tag_id
     end
   end
 end
