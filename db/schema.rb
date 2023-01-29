314501a9d23321974ee5846b1f5a97d59641d924
Enock <enock.mokua@student.moringaschool.com>
2023-01-29 20:38:58 +0300

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_29_170343) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_29_173328) do
   create_table "Projects_Tags", id: false, force: :cascade do |t|
     t.integer "Project_id", null: false
     t.integer "Tag_id", null: false
@@ -28,6 +28,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_29_170343) do
   end
 
   create_table "education_skills", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "education_units", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -120,6 +125,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_29_170343) do
   end
 
   create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "units", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.datetime "created_at", null: false
