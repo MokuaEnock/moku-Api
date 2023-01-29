de6ad96e46d59e5f82ea506835d8c5e3d2d24f00
Enock <enock.mokua@student.moringaschool.com>
2023-01-29 19:22:49 +0300

    add attributes to certificate

--- db/migrate/20230129160709_create_certifications.rb
+++ db/migrate/20230129160709_create_certifications.rb
@@ -1,6 +1,10 @@
 class CreateCertifications < ActiveRecord::Migration[7.0]
   def change
     create_table :certifications do |t|
+      t.string :name
+      t.string :institution
+      t.string :certificate_link
+      t.string :issue_date
 
       t.timestamps
     end
