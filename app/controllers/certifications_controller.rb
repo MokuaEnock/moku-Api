d83e591a4771a1f3a2f8a10575fdee82a61b1796
Enock <enock.mokua@student.moringaschool.com>
2023-01-29 19:31:12 +0300

    create actions for certificate controller

--- app/controllers/certifications_controller.rb
+++ app/controllers/certifications_controller.rb
@@ -1,2 +1,20 @@
 class CertificationsController < ApplicationController
+  def index
+    render json: Certification.all
+  end
+
+  def show
+    cert = Certification.find(params[:id])
+    render json: cert
+  end
+
+  def create
+    cert = Certification.create(cert_params)
+  end
+
+  private
+
+  def cert_params
+    params.permit(:name, :institution, :certificate_link, :issue_date)
+  end
 end
