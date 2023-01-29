eb518b742c5f791798df5a0ccca79d56f9b5b700
Enock <enock.mokua@student.moringaschool.com>
2023-01-29 20:21:39 +0300

    add attributes to education migration

--- db/migrate/20230129170343_create_educations.rb
+++ db/migrate/20230129170343_create_educations.rb
@@ -1,6 +1,11 @@
 class CreateEducations < ActiveRecord::Migration[7.0]
   def change
     create_table :educations do |t|
+      t.string :institution
+      t.date :start
+      t.date :end
+      t.string :name
+      t.string :major
 
       t.timestamps
     end
